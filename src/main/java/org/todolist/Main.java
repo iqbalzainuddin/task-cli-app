e325557b717febf922350b88f22b49ec8e118082
iqbalmzainuddin <ext_iqbal.mohammadzainuddin@malaysiaairlines.com>
2025-08-02 20:21:07 +0800

    feat: call input handler in main

--- src/main/java/org/todolist/Main.java
+++ src/main/java/org/todolist/Main.java
@@ -1,14 +1,17 @@
 package org.todolist;
 
+import org.todolist.DataService.InputHandler;
+
 public class Main {
     public static void main(String[] args) {
         try {
             System.out.println("==============");
             System.out.println("To-do List App");
             System.out.println("==============\n");
-        } catch (Exception e) {
+
+            InputHandler.performAction(args);
+        } catch (RuntimeException e) {
             System.out.println(e.getMessage());
-            e.printStackTrace();
         }
     }
 }
